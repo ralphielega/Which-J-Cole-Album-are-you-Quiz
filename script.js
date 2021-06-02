91c8f48dd795e49b1a9d4866dda7f4d0a367bb71
ralphielega <69807998+ralphielega@users.noreply.github.com>
2021-06-02 16:38:29 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(".dollar").click(function() {
     if(age<=25 && music==="beat"){
        $(".warm"). show();
         $(".results"). text("The Warm Up");
-    } else if (age<=25 && music==="words") {
+    } else if (age<=25 && music==="lyrics") {
            $(".sinner").show();
         $(".results").text("Born Sinner");
     } 
@@ -14,16 +14,20 @@ $(".dollar").click(function() {
             $(".results").text("K.O.D");
             $(".kod").show();
         }
-        else if (age>25 && music==="words") {
+        else if (age>25 && music==="lyrics") {
             $(".results").text("4 Your Eyes Only");
             $(".youreyes").show();
         }
         else {
             $(".results").text("try again");
+            $(".no").show();
 }
    
    $(".results").append("<p> You are " + age + " years old and you like music with an exceptional " + music + ".</p>");
     $(".results").show();
     count = count + 1;
     $(".counter").text("This quiz has been taken " + count + " times");
+});
+$(".dollar").click(function() {
+    $(".dreamville").css("width","700px");
 });
